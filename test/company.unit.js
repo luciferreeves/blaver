b40c9839b4f0f344c8c7d5deac828a81e34cd38f
Priyansh <bobbyskhs@gmail.com>
2022-01-06 17:49:41 -0500

    Passing tests for company.unit.js

--- test/company.unit.js
+++ test/company.unit.js
@@ -1,49 +1,50 @@
-if (typeof module !== "undefined") {
-  var assert = require("assert");
-  var sinon = require("sinon");
-  var bluffmaster = require("../index");
+if (typeof module !== 'undefined') {
+  var assert = require('assert');
+  var sinon = require('sinon');
+  var bluffmaster = require('../index');
 }
 
 describe("company.js", function () {
   describe("companyName()", function () {
+
     it("sometimes returns three last names", function () {
-      sinon.spy(bluffmaster.name, "lastName");
-      sinon.stub(bluffmaster.random, "number").returns(2);
+      sinon.spy(bluffmaster.name, 'lastName');
+      sinon.stub(bluffmaster.datatype, 'number').returns(2);
       var name = bluffmaster.company.companyName();
-      var parts = name.split(" ");
+      var parts = name.split(' ');
 
       assert.strictEqual(parts.length, 4); // account for word 'and'
       assert.ok(bluffmaster.name.lastName.calledThrice);
 
-      bluffmaster.random.number.restore();
+      bluffmaster.datatype.number.restore();
       bluffmaster.name.lastName.restore();
     });
 
     it("sometimes returns two last names separated by a hyphen", function () {
-      sinon.spy(bluffmaster.name, "lastName");
-      sinon.stub(bluffmaster.random, "number").returns(1);
+      sinon.spy(bluffmaster.name, 'lastName');
+      sinon.stub(bluffmaster.datatype, 'number').returns(1);
       var name = bluffmaster.company.companyName();
-      var parts = name.split("-");
+      var parts = name.split('-');
 
       assert.ok(parts.length >= 2);
       assert.ok(bluffmaster.name.lastName.calledTwice);
 
-      bluffmaster.random.number.restore();
+      bluffmaster.datatype.number.restore();
       bluffmaster.name.lastName.restore();
     });
 
     it("sometimes returns a last name with a company suffix", function () {
-      sinon.spy(bluffmaster.company, "companySuffix");
-      sinon.spy(bluffmaster.name, "lastName");
-      sinon.stub(bluffmaster.random, "number").returns(0);
+      sinon.spy(bluffmaster.company, 'companySuffix');
+      sinon.spy(bluffmaster.name, 'lastName');
+      sinon.stub(bluffmaster.datatype, 'number').returns(0);
       var name = bluffmaster.company.companyName();
-      var parts = name.split(" ");
+      var parts = name.split(' ');
 
       assert.ok(parts.length >= 2);
       assert.ok(bluffmaster.name.lastName.calledOnce);
       assert.ok(bluffmaster.company.companySuffix.calledOnce);
 
-      bluffmaster.random.number.restore();
+      bluffmaster.datatype.number.restore();
       bluffmaster.name.lastName.restore();
       bluffmaster.company.companySuffix.restore();
     });
@@ -58,13 +59,13 @@ describe("company.js", function () {
 
   describe("catchPhrase()", function () {
     it("returns phrase comprising of a catch phrase adjective, descriptor, and noun", function () {
-      sinon.spy(bluffmaster.random, "arrayElement");
-      sinon.spy(bluffmaster.company, "catchPhraseAdjective");
-      sinon.spy(bluffmaster.company, "catchPhraseDescriptor");
-      sinon.spy(bluffmaster.company, "catchPhraseNoun");
+      sinon.spy(bluffmaster.random, 'arrayElement');
+      sinon.spy(bluffmaster.company, 'catchPhraseAdjective');
+      sinon.spy(bluffmaster.company, 'catchPhraseDescriptor');
+      sinon.spy(bluffmaster.company, 'catchPhraseNoun');
       var phrase = bluffmaster.company.catchPhrase();
 
-      assert.ok(phrase.split(" ").length >= 3);
+      assert.ok(phrase.split(' ').length >= 3);
       assert.ok(bluffmaster.random.arrayElement.calledThrice);
       assert.ok(bluffmaster.company.catchPhraseAdjective.calledOnce);
       assert.ok(bluffmaster.company.catchPhraseDescriptor.calledOnce);
@@ -79,19 +80,19 @@ describe("company.js", function () {
 
   describe("bs()", function () {
     it("returns phrase comprising of a BS buzz, adjective, and noun", function () {
-      sinon.spy(bluffmaster.random, "arrayElement");
-      sinon.spy(bluffmaster.company, "bsBuzz");
-      sinon.spy(bluffmaster.company, "bsAdjective");
-      sinon.spy(bluffmaster.company, "bsNoun");
+      sinon.spy(bluffmaster.random, 'arrayElement');
+      sinon.spy(bluffmaster.company, 'bsBuzz');
+      sinon.spy(bluffmaster.company, 'bsAdjective');
+      sinon.spy(bluffmaster.company, 'bsNoun');
       var bs = bluffmaster.company.bs();
 
-      assert.ok(typeof bs === "string");
+      assert.ok(typeof bs === 'string');
       assert.ok(bluffmaster.random.arrayElement.calledThrice);
       assert.ok(bluffmaster.company.bsBuzz.calledOnce);
       assert.ok(bluffmaster.company.bsAdjective.calledOnce);
       assert.ok(bluffmaster.company.bsNoun.calledOnce);
 
-      bluffmaster.random.arrayElement.restore();
+      bluffmaster.random.arrayElement.restore();        
       bluffmaster.company.bsBuzz.restore();
       bluffmaster.company.bsAdjective.restore();
       bluffmaster.company.bsNoun.restore();
